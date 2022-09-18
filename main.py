5ab989370adc13adc517acf4b8a017c73ca3c358
xueruz <xue.zhou777@gmail.com>
2022-09-18 08:45:33 -0400

    Add functions and try lin reg

--- main.py
+++ main.py
@@ -6,20 +6,82 @@ from sklearn.model_selection import train_test_split
 
 # Organize and scale prostate cancer data 
 
-# read data in from the txt file
-df = pd.read_csv('./prostatedata.txt', delimiter = "\t")
+# Read data in from the txt file
+df = pd.read_csv('prostatedata.txt', delimiter = "\t")
 
-# separate into inputs and outputs 
-x = pd.DataFrame(df, columns = ['lcavol','lweight','age','lbph','svi','lcp','gleason','pgg45'])
+# Pad first column with 1's 
+df.insert(0, 'intercept', 1)
+
+# Separate into inputs and outputs 
+X = pd.DataFrame(df, columns = ['intercept', 'lcavol','lweight','age','lbph','svi','lcp','gleason','pgg45'])
 y = pd.DataFrame(df, columns = ['lpsa'])
 
-# scale features -- standard scaling 
+# Scale features -- standard scaling 
 ss = StandardScaler()
-data_scaled = ss.fit_transform(x)
+data_scaled = ss.fit_transform(X)
 
-# split data into 80% train, 10% validate, 10% train 
-train_x, rest_x, train_y, rest_y = train_test_split(x, y, test_size = 0.2) 
+# Split data into 80% train, 10% validate, 10% train 
+train_x, rest_x, train_y, rest_y = train_test_split(X, y, test_size = 0.2) 
 val_x, test_x, val_y, test_y = train_test_split(rest_x, rest_y, test_size = 0.5)
+
+# Convert to Numpy arrays
+train_x = train_x.to_numpy() 
+test_x = test_x.to_numpy()
+train_y = train_y.to_numpy()
+test_y = test_y.to_numpy()
+
+#Functions 
+
+# Find betas (weights)
+def train_data(X, y):
+  beta_hat = np.linalg.inv(X.T @ X) @ X.T @ y # eq 3.6
+  return beta_hat
+
+# Use betas to test data output 
+def test_data(X, beta_hat): 
+  y_hat = X @ beta_hat # eq 3.7
+  return y_hat
+
+# Get MSE (mean squared error)
+def get_MSE(y, y_hat):
+  diff_y = np.square(y-y_hat)
+  mse = (np.sum(diff_y))/len(y)
+  return mse 
+
+# Estimate var_hat 
+def est_var_hat(y, y_hat, X):
+  diff_y = np.square(y-y_hat)
+  var_hat = (np.sum(diff_y))/(len(y)-len(X[0]) - 1 - 1) # eq 3.8
+  return var_hat
+
+# Get standard errors
+def get_std_errors(var_hat, diag_vals): 
+   std_errs = var_hat * np.sqrt(diag_vals)
+   return std_errs
+
+# Get Z-scores 
+def get_z_scores(beta_hat, std_errs):
+  z_scores = np.zeros(len(beta_hat))
+  for j in range(len(beta_hat)): # eq 3.12
+    z_scores[j] = beta_hat[j]/std_errs[j]
+    return z_scores
+
+#1) Linear regression 
+
+beta_hat = train_data(train_x, train_y)
+print(beta_hat)
+y_hat = test_data(test_x, beta_hat)
+mse = get_MSE(test_y, y_hat)
+
+print("Mean squared error: ", mse)
+
+# Get correlation coefficients
+
+corr_coeffs = np.corrcoef(X_features); 
+
+# note: insert table 3.1 with correlations 
+
+# note: insert table 3.2 with beta_hat, std_errs, z_scores 
 
 # Replicate the analysis from chapter 3 of this dataset. Divide your data into roughly 80% train, 10% validation, 
 # 10% test. You must keep this split for all 3 parts of this assignment in order to compare the methods fairly. 
